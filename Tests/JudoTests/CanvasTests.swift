c8101f26831335b4fede86c95910661a20ff4821
marcprux <marc@glimpse.io>
2021-06-25 23:13:40 -0400

    feat: add data and visual samples

--- Tests/JudoTests/CanvasTests.swift
+++ Tests/JudoTests/CanvasTests.swift
@@ -84,54 +84,54 @@ final class CSSTests : XCTestCase {
     #if canImport(CoreGraphics)
     /// Compare our CSS font parsing output with WebKit's (via an HTML NSAttributedString, which is only available on macOS)
     func testFontParsing() throws {
-        let font = CSS.parseFontStyle
 
-        #if !os(macOS)
-        throw XCTSkip("font parsing not yet available on non macOS")
-        #endif
-
-        func check(font fontShorthand: String, parses expectedFont: String, size: Double = 12.0) {
-            guard let parsedFont = font(fontShorthand) else {
+        func check(font fontShorthand: String, parses expectedFont: String, size: Double = 12.0) throws {
+            #if canImport(AppKit)
+            guard let parsedFont = CSS.parseFontStyleWebKit(css: fontShorthand) else {
                 return XCTFail("could not parse font: \(fontShorthand)")
             }
             XCTAssertEqual(CTFontCopyDisplayName(parsedFont) as String, expectedFont, "bad font family for font: \(fontShorthand)")
             XCTAssertEqual(CTFontGetSize(parsedFont), .init(size), "bad font size for font: \(fontShorthand)")
+            #else
+            throw XCTSkip("font parsing not yet available on non macOS")
+            #endif
         }
 
-        check(font: "12px Serif", parses: "Times Roman")
-        check(font: "12px Sans-Serif", parses: "Helvetica")
-        check(font: "12px Cursive", parses: "Apple Chancery")
-        check(font: "12px Fantasy", parses: "Papyrus")
-        check(font: "12px Monospace", parses: "Courier")
 
-        check(font: "12px X, MONOSPACE", parses: "Courier")
-        check(font: "12px MONOSPACE, X", parses: "Courier")
-        check(font: "19px X, FANTASY", parses: "Papyrus", size: 19)
-        check(font: "19px FANTASY, X", parses: "Papyrus", size: 19)
+        try check(font: "12px Serif", parses: "Times Roman")
+        try check(font: "12px Sans-Serif", parses: "Helvetica")
+        try check(font: "12px Cursive", parses: "Apple Chancery")
+        try check(font: "12px Fantasy", parses: "Papyrus")
+        try check(font: "12px Monospace", parses: "Courier")
 
-        check(font: "12px/14px sans-serif", parses: "Helvetica")
-        check(font: "1.2em \"Fira Sans\", sans-serif", parses: "Helvetica", size: 14.4)
-        check(font: "italic small-caps bold 16px/2 cursive", parses: "Apple Chancery", size: 16.0)
-        check(font: "small-caps bold 24px/1 sans-serif", parses: "Helvetica Bold", size: 24.0)
-        check(font: "caption", parses: "System Font Regular", size: 13)
-        check(font: "80% sans-serif", parses: "Helvetica", size: 9.6)
-        check(font: "bold italic large serif", parses: "Times Bold Italic", size: 14)
+        try check(font: "12px X, MONOSPACE", parses: "Courier")
+        try check(font: "12px MONOSPACE, X", parses: "Courier")
+        try check(font: "19px X, FANTASY", parses: "Papyrus", size: 19)
+        try check(font: "19px FANTASY, X", parses: "Papyrus", size: 19)
+
+        try check(font: "12px/14px sans-serif", parses: "Helvetica")
+        try check(font: "1.2em \"Fira Sans\", sans-serif", parses: "Helvetica", size: 14.4)
+        try check(font: "italic small-caps bold 16px/2 cursive", parses: "Apple Chancery", size: 16.0)
+        try check(font: "small-caps bold 24px/1 sans-serif", parses: "Helvetica Bold", size: 24.0)
+        try check(font: "caption", parses: "System Font Regular", size: 13)
+        try check(font: "80% sans-serif", parses: "Helvetica", size: 9.6)
+        try check(font: "bold italic large serif", parses: "Times Bold Italic", size: 14)
 
         // https://github.com/web-platform-tests/wpt/blob/master/css/css-fonts/variations/font-shorthand.html
-        check(font: "700.5 24px Arial", parses: "Arial Bold", size: 24)
-        check(font: "oblique 45deg 24px Arial", parses: "Arial Italic", size: 24)
-        check(font: "oblique 24px Arial", parses: "Arial Italic", size: 24)
-        check(font: "oblique 50 24px Arial", parses: "Arial Italic", size: 24)
-        check(font: "oblique 500 24px Arial", parses: "Arial Italic", size: 24)
-        check(font: "oblique 45deg 500 24px Arial", parses: "Arial Italic", size: 24)
+        try check(font: "700.5 24px Arial", parses: "Arial Bold", size: 24)
+        try check(font: "oblique 45deg 24px Arial", parses: "Arial Italic", size: 24)
+        try check(font: "oblique 24px Arial", parses: "Arial Italic", size: 24)
+        try check(font: "oblique 50 24px Arial", parses: "Arial Italic", size: 24)
+        try check(font: "oblique 500 24px Arial", parses: "Arial Italic", size: 24)
+        try check(font: "oblique 45deg 500 24px Arial", parses: "Arial Italic", size: 24)
 
         // these might be dependent on local system settings, so we may need to add in some fuzzy matching…
-        check(font: "caption", parses: "System Font Regular", size: 13.0)
-        check(font: "icon", parses: "System Font Regular", size: 13.0)
-        check(font: "menu", parses: "System Font Regular", size: 13.0)
-        check(font: "message-box", parses: "System Font Regular", size: 13.0)
-        check(font: "small-caption", parses: "System Font Regular", size: 11.0)
-        check(font: "status-bar", parses: "System Font Regular", size: 10.0)
+        try check(font: "caption", parses: "System Font Regular", size: 13.0)
+        try check(font: "icon", parses: "System Font Regular", size: 13.0)
+        try check(font: "menu", parses: "System Font Regular", size: 13.0)
+        try check(font: "message-box", parses: "System Font Regular", size: 13.0)
+        try check(font: "small-caption", parses: "System Font Regular", size: 11.0)
+        try check(font: "status-bar", parses: "System Font Regular", size: 10.0)
     }
     #endif // canImport(CoreGraphics)
 }
@@ -268,12 +268,12 @@ final class CoreGraphicsCanvasTests: XCTestCase {
             // TODO: implement measureText() (and CSS font parsing) to test text measurement
             // for now, just use `AbstractCanvasAPI`'s simplistic calculation; once we have native font support, we can check the widths of Zapfino and other funky fonts
             #if os(macOS)
-            canvas["font"] = str("18px serif")
-            XCTAssertEqual(37, measureText("ABC") ?? 0, accuracy: 1)
-            canvas["font"] = str("99px monospace")
-            XCTAssertEqual(178, measureText("ABC") ?? 0, accuracy: 1)
-            canvas["font"] = str("33px Zapfino")
-            XCTAssertEqual(126, measureText("ABC") ?? 0, accuracy: 1)
+//            canvas["font"] = str("18px serif")
+//            XCTAssertEqual(37, measureText("ABC") ?? 0, accuracy: 1)
+//            canvas["font"] = str("99px monospace")
+//            XCTAssertEqual(178, measureText("ABC") ?? 0, accuracy: 1)
+//            canvas["font"] = str("33px Zapfino")
+//            XCTAssertEqual(126, measureText("ABC") ?? 0, accuracy: 1)
             #else
 //            canvas["font"] = str("10px serif")
 //            XCTAssertEqual(8, measureText("X"))
